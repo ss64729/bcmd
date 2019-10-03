18fb591c3d09b840ab529c4954321f5a6f8651d1
Sarah Sizemore <sarah.sizemore@kroger.com>
2019-10-03 11:04:23 -0400

    commit first

---
+++ bmdb-queries.sql
@@ -0,0 +1,30 @@
+select * from movie;
+select * from actor;
+select * from credit;
+
+select title, firstname, lastname, role from 
+movie a, actor b, credit c where
+a.movieid = c.movieid and
+b.actorid = c.actorid;
+
+select title, firstname, lastname, role from movie a
+join credit c on a.movieid = c.movieid
+join actor b on c.actorid = b.actorid;
+
+select title, concat(firstname," ", lastname) as actor, role from movie a
+join credit c on a.movieid = c.movieid
+join actor b on c.actorid = b.actorid;
+
+
+select * from movie where title like "%Hidden%";
+
+select firstname, lastname from actor where
+actorid in (select actorid from credit 
+where movieid = 5);
+
+select firstname, lastname, dob from actor where
+dob < '1973-08-28' order by 3 desc;
+
+select firstname, lastname from actor a
+join credit b on a.actorid = b.actorid
+where movieid = '4'
